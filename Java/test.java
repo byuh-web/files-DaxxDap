f9186cf990a75fabd09165a3fed4bd6fa0abf9bf
DaxxDap <daceryl.daplin@gmail.com>
2026-05-16 21:43:18 -1000

    update

---
+++ Java/test.java
@@ -0,0 +1,27 @@
+package Java;
+
+public class test {
+    public static void main(String[] args) {
+       
+          // Your code starts here
+          
+        int x = 5;
+
+        if (x == 5) {
+            System.out.println("Equal");
+        } else {
+            System.out.println("Not equal"); 
+
+            }
+            
+
+for (int i = 0; i < 3; i++) {
+
+    
+    System.out.println("Level " + i);
+
+
+        }
+    }
+
+}
